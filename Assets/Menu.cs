09def4120a082d98a91c63491a6b2179f546a1dc
MarcoRamirezGT <46507192+MarcoRamirezGT@users.noreply.github.com>
2020-02-02 17:47:57 -0600

    Initial commit

---
+++ Assets/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+   
+    public void Jugar()
+    {
+        
+        SceneManager.LoadScene("Jugar");
+       
+        Time.timeScale = 1f;
+    }
+    public void Salir()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+}
